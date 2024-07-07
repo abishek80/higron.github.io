f5fa20a7c91297464d795cff7a8d9daf0a3a9c80
Antony Abishek <antony.abishek@venpep.com>
2024-07-07 21:52:10 +0530

    counter section added

--- js/script.js
+++ js/script.js
@@ -53,3 +53,21 @@ $(document).ready(function () {
         }
     });
 });
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+        const increment = target / 100;
+        if (current < target) {
+            counter.innerText = `${Math.ceil(current + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    }
+    updateCounter();
+});
